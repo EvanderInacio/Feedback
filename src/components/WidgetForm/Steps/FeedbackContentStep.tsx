119fe332f7f0dcba5feb2f9111c0d7d5608af944
Evander Inacio <evander.20112@hotmail.com>
2022-08-13 11:19:02 -0300

    Add Function Screenshot :camera_flash:

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft, Camera } from "phosphor-react"
+import { useState } from "react"
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
+import { ScreenshotButton } from "../ScreenshotButton"
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType
@@ -11,6 +13,7 @@ export function FeedbackContentStep({
   feedbackType, 
   onFeedbackRestartRequested 
 }: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null)
 
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
@@ -45,12 +48,10 @@ export function FeedbackContentStep({
         />
 
         <footer className="flex gap-2 mt-2">
-          <button
-            type="button"
-            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none text-slate-100 focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-          >
-            <Camera className="w-6 h-6"/>
-          </button>
+          <ScreenshotButton 
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
 
           <button
             type="submit"
